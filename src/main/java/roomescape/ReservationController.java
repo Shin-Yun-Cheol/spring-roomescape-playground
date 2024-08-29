5779a31f5bab65367148cb6857e035a70c6295c2
Shin Yuncheol <yptlsdbscjf@naver.com>
2024-08-29 23:06:23 +0900

    test 10 (계층화 리팩터링 : test코드 추가, 기존의 ReservationCOntroller를 ReservationDAO와 ReservationService로 분리해서 리팩터링했다.)

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -2,92 +2,38 @@ package roomescape;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.*;
-import roomescape.exception.InvalidReservationException;
-import roomescape.exception.NotFoundException;
+
 
 import java.net.URI;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final ReservationService reservationService;
 
     @Autowired
-    public ReservationController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
     }
 
-    @GetMapping("/reservations")
-    @ResponseBody
+    @GetMapping
     public ResponseEntity<List<Reservation>> readReservations() {
-        String sql = "SELECT r.id AS reservation_id, r.name, r.date, t.id AS time_id, t.time AS time_value FROM reservation AS r INNER JOIN time AS t ON r.time_id = t.id";
-        List<Reservation> reservations = jdbcTemplate.query(sql, new ReservationRowMapper());
+        List<Reservation> reservations = reservationService.getAllReservations();
         return ResponseEntity.ok().body(reservations);
     }
 
-
     @PostMapping
     public ResponseEntity<Void> createReservation(@RequestBody ReservationRequest reservationRequest) {
-        if (reservationRequest.getName() == null || reservationRequest.getName().isEmpty() ||
-                reservationRequest.getDate() == null || reservationRequest.getDate().isEmpty() ||
-                reservationRequest.getTimeId() == null) {
-            throw new InvalidReservationException("Invalid reservation details");
-        }
-
-        String sql = "INSERT INTO reservation (name, date, time_id) VALUES (?, ?, ?)";
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1, reservationRequest.getName());
-            ps.setString(2, reservationRequest.getDate());
-            ps.setLong(3, reservationRequest.getTimeId());
-            return ps;
-        }, keyHolder);
-
-        Number key = keyHolder.getKey();
-        URI location = URI.create("/reservations/" + key.longValue());
-        return ResponseEntity.created(location).build();
+        reservationService.createReservation(reservationRequest);
+        return ResponseEntity.created(URI.create("/reservations/")).build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable long id) {
-        String sql = "DELETE FROM reservation WHERE id = ?";
-        int rowsAffected = jdbcTemplate.update(sql, id);
-
-        if (rowsAffected > 0) {
-            return ResponseEntity.noContent().build();
-        } else {
-            throw new NotFoundException("Reservation not found");
-        }
+        reservationService.deleteReservation(id);
+        return ResponseEntity.noContent().build();
     }
-
-    // RowMapper for Reservation
-    private static class ReservationRowMapper implements RowMapper<Reservation> {
-        @Override
-        public Reservation mapRow(ResultSet rs, int rowNum) throws SQLException {
-            Time time = new Time(
-                    rs.getLong("time_id"),
-                    rs.getString("time_value")
-            );
-
-            return new Reservation(
-                    rs.getLong("reservation_id"),
-                    rs.getString("name"),
-                    rs.getString("date"),
-                    time
-            );
-        }
-    }
-
 }
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -183,19 +184,36 @@ public class MissionStepTest {
 //                .statusCode(204);
 //    }
 
-    @Test
-    void 구단계() {
-        Map<String, String> reservation = new HashMap<>();
-        reservation.put("name", "브라운");
-        reservation.put("date", "2023-08-05");
-        reservation.put("time", "10:00");
+//    @Test
+//    void 구단계() {
+//        Map<String, String> reservation = new HashMap<>();
+//        reservation.put("name", "브라운");
+//        reservation.put("date", "2023-08-05");
+//        reservation.put("time", "10:00");
+//
+//        RestAssured.given().log().all()
+//                .contentType(ContentType.JSON)
+//                .body(reservation)
+//                .when().post("/reservations")
+//                .then().log().all()
+//                .statusCode(400);
+//    }
 
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(reservation)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(400);
+    @Autowired
+    private ReservationController reservationController;
+
+    @Test
+    void 십단계() {
+        boolean isJdbcTemplateInjected = false;
+
+        for (Field field : reservationController.getClass().getDeclaredFields()) {
+            if (field.getType().equals(JdbcTemplate.class)) {
+                isJdbcTemplateInjected = true;
+                break;
+            }
+        }
+
+        assertThat(isJdbcTemplateInjected).isFalse();
     }
 
 
